99c2b66a9653b64a9d5ce2dcb250fe16fcc002cb
Sutheesh Sukumaran <sutheeshbose@hotmail.com>
2019-02-16 21:35:51 -0600

    Initial Commit

---
+++ Screenshot Builder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Screenshot Builder
+//
+//  Created by Sutheesh Sukumaran on 2/16/19.
+//  Copyright © 2019 iLabbs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Screenshot Builder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Screenshot Builder
+//
+//  Created by Sutheesh Sukumaran on 2/16/19.
+//  Copyright © 2019 iLabbs. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
